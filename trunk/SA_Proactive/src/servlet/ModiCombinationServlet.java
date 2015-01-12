4f8241985ccb021d9e83d77d5a55178e4f368e04
justabc12311@gmail.com <justabc12311@gmail.com@274008d6-78ec-8349-4ef6-ad5ec3662611>
2015-01-12 16:00:22 +0000

    modiCombination.jsp CombinationDetailDB ModiCombinationServlet

--- trunk/SA_Proactive/src/servlet/ModiCombinationServlet.java
+++ trunk/SA_Proactive/src/servlet/ModiCombinationServlet.java
@@ -1,19 +1,21 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import database.CartDB;
+
+
 import database.CombinationDB;
 import database.CombinationDetailDB;
 import database.ProductDB;
 import bean.CombinationBean;
 import bean.CombinationDetailBean;
-import bean.MemberBean;
 import bean.ProductBean;
 
 public class ModiCombinationServlet extends HttpServlet{
@@ -22,11 +24,26 @@ public class ModiCombinationServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 				req.setCharacterEncoding("UTF-8");
-				ProductBean productBean = new ProductBean();
-			
 				String combinationID = req.getParameter("combinationID");
-				String productID = req.getParameter(productBean.getProductType());
-								
+				
+				
+				
+				String[] productTypeList = new String[9];
+				productTypeList[0]="中央處理器";
+				productTypeList[1]="主機板";
+				productTypeList[2]="硬碟/SSD";
+				productTypeList[3]="記憶體";
+				productTypeList[4]="顯示/繪圖卡";
+				productTypeList[5]="電源供應器";
+				productTypeList[6]="電腦機殼";
+				productTypeList[7]="DVD燒錄器";
+				productTypeList[8]="CPU散熱風扇";
+				
+				for(int i=0;i<=8;i++){
+				String productID = req.getParameter(productTypeList[i]);
+						
+				
+				
 				
 				CombinationDetailDB combinationDetailDB = new CombinationDetailDB();
 				CombinationDetailBean combinationDetailBean = new CombinationDetailBean();
@@ -40,6 +57,8 @@ public class ModiCombinationServlet extends HttpServlet{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				}
+				
 				
 				
 				
